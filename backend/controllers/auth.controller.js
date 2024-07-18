8859419d928ba3a1558bfa93302bef1da68a052f
Mistire <mdwithgod@gmail.com>
2024-07-19 00:24:09 +0300

    1-project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+  res.json({
+    message: "You hit the signup end point",
+  })
+}
+
+export const login = async (req, res) => {
+  res.json({
+    message: "You hit the signup end point",
+  })
+}
+
+export const logout = async (req, res) => {
+  res.json({
+    message: "You hit the signup end point",
+  })
+}
